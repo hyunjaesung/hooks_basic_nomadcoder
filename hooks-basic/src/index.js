0bd5fd93770750d96c9ec462e10a91f574d89373
steve <steveyhjsung@gmail.com>
2019-06-17 18:26:12 +0900

    useInput1

--- hooks-basic/src/index.js
+++ hooks-basic/src/index.js
@@ -3,18 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+const useInput = (initialValue) => {
+	const [ value, setValue ] = useState(initialValue);
+	const onChange = (event) => {
+		const { target: { value } } = event;
+		setValue(value);
+	};
+
+	return { value, onChange };
+};
+
 const App = () => {
-	const [ item, setItem ] = useState(1);
+	const name = useInput('Mr.');
+	// 초기값을 이용한 첫번째 값과 두번째인 셋함수를 품고있는 onChange 함수를 name 안에 넣어줌
+	return (
+		<div className="App">
+			<h1>Hello</h1>
+			<input placeholder="Name" value={name.value} onChange={name.onChange} />
 
-    const onIncrement = ()=>setItem(item+1);
-
-
-    return (
-        <>
-        <div> {item}</div>
-        <button onClick = {onIncrement}>+</button>
-        </>
-        );
+			{/* 그냥 ...name 쓰면 value ={..} onChange ={..} 대체가능  */}
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
