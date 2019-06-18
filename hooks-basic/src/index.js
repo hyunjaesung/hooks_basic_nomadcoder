33bf28b23abf97090d1e0cc7011717c2d1d0729b
steve <steveyhjsung@gmail.com>
2019-06-18 19:00:33 +0900

    useTitle

--- hooks-basic/src/index.js
+++ hooks-basic/src/index.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+const useTitle = (initialTitle) => {
+	const [ title, setTitle ] = useState(initialTitle);
+	const updateTitle = () => {
+		const htmlTitle = document.querySelector('title');
+		htmlTitle.innerText = title;
+	};
+	useEffect(updateTitle, [ title ]);
+	// title 이 마운트완료 되거나 업데이트되면 함수실행
+	return setTitle;
+};
+
 const App = () => {
-	return <div className="App" />;
+	const titleUpdater = useTitle('Loading...');
+	setTimeout(() => titleUpdater('Home'), 5000);
+	return (
+		<div className="App">
+			<div>Hi</div>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
