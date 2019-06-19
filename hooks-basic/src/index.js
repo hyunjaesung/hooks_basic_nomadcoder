6c3933710d6659149449f59819caec1241fc87ba
stevesung <steveysung91@gmail.com>
2019-06-19 12:00:25 +0900

    useFadeIn

--- hooks-basic/src/index.js
+++ hooks-basic/src/index.js
@@ -1,30 +1,34 @@
-import React, { useState, useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-const usePreventLeave = () => {
-	const listener = (event) => {
-		event.preventDefault();
-		event.returnValue = ''; // 애가있어야지 크롬에서 작동이됨
-	};
-	const enablePrevent = () => window.addEventListener('beforeunload', listener);
-	//beforeunload 이벤트는 윈도우가 닫히기 전에 함수실행시킨다
-	const disablePrevent = () => window.removeEventListener('beforeunload', listener);
-	return { enablePrevent, disablePrevent };
+const useFadeIn = (duration = 1) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      // css 넣기
+      current.style.transition = `opacity ${duration}s`;
+      current.style.opacity = 1;
+    }
+  }, []);
+
+  return { ref: element, style: { opacity: 0 } };
 };
+
 const App = () => {
-	const { enablePrevent, disablePrevent } = usePreventLeave();
-	return (
-		<div className="App">
-			<h1>Hi</h1>
-			<button onClick={enablePrevent}>Protect</button>
-			<button onClick={disablePrevent}>Unprotect</button>
-		</div>
-	);
+  const fadeInH1 = useFadeIn(1);
+  const fadeInP = useFadeIn(4);
+  return (
+    <div className="App">
+      <h1 {...fadeInH1}>Hi</h1>
+      <p {...fadeInP}> blah blah balh </p>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
