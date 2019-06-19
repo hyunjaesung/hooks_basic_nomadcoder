bafc170158b33fea088bc1a424c1aff8653760d3
stevesung <steveysung91@gmail.com>
2019-06-19 19:58:09 +0900

    useFullscreen

--- hooks-basic/src/index.js
+++ hooks-basic/src/index.js
@@ -2,28 +2,45 @@ import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import { callbackify } from "util";
 
-const useFadeIn = (duration = 1) => {
+const useFullscreen = onFullS => {
   const element = useRef();
-  useEffect(() => {
-    if (element.current) {
-      const { current } = element;
-      // css 넣기
-      current.style.transition = `opacity ${duration}s`;
-      current.style.opacity = 1;
-    }
-  }, []);
 
-  return { ref: element, style: { opacity: 0 } };
+  const triggerFull = () => {
+    if (element.current) {
+      element.current.requestFullscreen();
+      //  브라우저에 요청 함수
+      if (onFullS && typeof onFullS === "function") {
+        onFullS(true); // true 반환
+      }
+    }
+  };
+  const exitFull = () => {
+    document.exitFullscreen();
+    // 이상하지만 원래 이리쓴다
+    if (onFullS && typeof onFullS === "function") {
+      onFullS(false); // false 반환
+    }
+  };
+
+  return { element, triggerFull, exitFull };
 };
 
 const App = () => {
-  const fadeInH1 = useFadeIn(1);
-  const fadeInP = useFadeIn(4);
+  const onFullS = isFullS => {
+    console.log(isFullS ? "We are Full" : "We are not full");
+  };
+
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+
   return (
-    <div className="App">
-      <h1 {...fadeInH1}>Hi</h1>
-      <p {...fadeInP}> blah blah balh </p>
+    <div className="App" style={{ height: "4000px" }}>
+      <div ref={element}>
+        <img src="https://yt3.ggpht.com/a/AGF-l7-MAndAKQeGPRYBSvmImKM4r4075vRKnEwwIg=s88-mo-c-c0xffffffff-rj-k-no" />
+        <button onClick={triggerFull}>Make full screen</button>
+        <button onClick={exitFull}>Exit full screen</button>
+      </div>
     </div>
   );
 };
